e8132d66bda968a6c5fc516211f7a8fdb4e0a1d4
simon <sajmoooonsk@gmail.com>
2022-08-04 20:44:37 +0200

    feat: add player animation

--- script/sketch.js
+++ script/sketch.js
@@ -1,13 +1,30 @@
+var playerAnim;
 var player;
 
 function setup(){
     createCanvas(400,400);
 
-    player = createSprite(150, 150)
+    player = createSprite(150, 150);
+    player.addAnimation('walk',playerAnim)
 }
 
 function draw(){
-    background(200)
+    background(200);
 
     drawSprites();
+}
+
+function preload(){
+    playerAnim = loadAnimation('../assets/img/alien/p1_walk01.png',
+                                '../assets/img/alien/p1_walk02.png',
+                                '../assets/img/alien/p1_walk03.png',
+                                '../assets/img/alien/p1_walk04.png',
+                                '../assets/img/alien/p1_walk05.png',
+                                '../assets/img/alien/p1_walk06.png',
+                                '../assets/img/alien/p1_walk07.png',
+                                '../assets/img/alien/p1_walk08.png',
+                                '../assets/img/alien/p1_walk08.png',
+                                '../assets/img/alien/p1_walk09.png',
+                                '../assets/img/alien/p1_walk10.png',
+                                '../assets/img/alien/p1_walk11.png');
 }
